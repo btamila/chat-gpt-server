5a1a4908a4cfa0f2b236fb3f9266414008de1119
GMX\002TPV744 <bharathi.tamilanban.alagarsamy@ibm.com>
2023-06-28 06:36:06 +0530

    Key added11

--- index.js
+++ index.js
@@ -8,46 +8,35 @@ const port = process.env.PORT || 2990;
 const { Configuration, OpenAIApi } = require("openai");
 dotenv.config();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const configuration = new Configuration({
-//   apiKey: "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",
-// });
-// const openai = new OpenAIApi(configuration);
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
 
-// app.post("/", async (req, res) => {
-//   const { chats, model } = req.body;
-//   console.log(model);
-//   const response = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     prompt: `${chats}`,
-//     max_tokens: 1000,
-//     temperature: 1,
-//   });
-//   res.json({ chat: response.data.choices[0].text });
-// });
-
-// app.get("/", async (req, res) => {
-//   const response = await openai.listModels();
-//   // res.status(200).send({ model: response.data.data });
-//   res.json({ model: response.data.data });
-// });
+app.post("/", async (req, res) => {
+  const { chats, model } = req.body;
+  console.log(model);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${chats}`,
+    max_tokens: 1000,
+    temperature: 1,
+  });
+  res.json({ chat: response.data.choices[0].text });
+});
 
 app.get("/", async (req, res) => {
+  const response = await openai.listModels();
+  // res.status(200).send({ model: response.data.data });
+  res.json({ model: response.data.data });
+});
+
+app.get("/chat-gpt", async (req, res) => {
   res.status(200).send({ Message: "Hello world!" });
 });
 
